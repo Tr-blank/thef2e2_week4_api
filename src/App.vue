e8165e6a7d7969eff218abbee8f3ff1b12d7c662
tiya-chang <49636182+tiya-chang@users.noreply.github.com>
2019-08-12 13:05:01 +0800

    :zap: Feat: updata page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
   #app
     main
-      .step
-        span.step__item.step__item--activity 1
+      .step.step--first
+        span.step__item 1
         span.step__item 2
         span.step__item 3
       .main-container
         .order
           .order-container
-            .order__title
+            .order__title 訂單資訊
             p.order__info
               |商品名稱：
               br
@@ -21,29 +21,29 @@
               |訂單金額：
               br
               |NT$ 600
-          .button 返回商店
+          .order__button 返回商店
         .content
           h1.content__title STEP1. 選擇付款方式
           .radio-container
             input.radio__control(type="radio" name="shopAPI" id="card")
             label.radio__item(for="card")
-              img(src="./assets/images/credit-card.svg")
+              img.radio__icon(src="./assets/images/credit-card.svg")
               .radio__title 信用卡/金融卡
             input.radio__control(type="radio" name="shopAPI" id="unionpay")
             label.radio__item(for="unionpay")
-              img(src="./assets/images/unionpay.svg")
+              img.radio__icon(src="./assets/images/unionpay.svg")
               .radio__title 銀聯卡
             input.radio__control(type="radio" name="shopAPI" id="shop")
             label.radio__item(for="shop")
-              img(src="./assets/images/shop.svg")
+              img.radio__icon(src="./assets/images/shop.svg")
               .radio__title 超商付款
             input.radio__control(type="radio" name="shopAPI" id="webAtm")
             label.radio__item(for="webAtm")
-              img(src="./assets/images/web-atm.svg")
+              img.radio__icon(src="./assets/images/web-atm.svg")
               .radio__title Web ATM
             input.radio__control(type="radio" name="shopAPI" id="atm")
             label.radio__item(for="atm")
-              img(src="./assets/images/atm.svg")
+              img.radio__icon(src="./assets/images/atm.svg")
               .radio__title ATM 轉帳
           a.shop-button 下一步
 </template>
@@ -65,8 +65,29 @@ export default {
   -moz-osx-font-smoothing grayscale
   text-align center
   color #2c3e50
+main::after
+  content: '';
+  position: absolute;
+  width: 100%;
+  background: url(/img/footer-background.576d29f0.svg) #defff2 center 20px;
+  background-size: 60px;
+  height: 14vh;
+  display: block;
+  bottom: 0;
+  left: 0;
+  z-index: -1;
 .step
   padding: 40px;
+  &::after
+    content: '';
+    height: 2px;
+    width: 266px;
+    background: #eee;
+    position: absolute;
+    left: 50%;
+    margin-left: -133px;
+    top: 60px;
+    z-index: -1;
   &__item
     width: 40px;
     line-height: 40px;
@@ -77,8 +98,11 @@ export default {
     background: #D8D8D8;
     font-size: 20px;
     font-family: NotoSansCJKtc-Medium;
-    &--activity
+  &--first
+    ^[-1]__item:nth-child(1)
       background: #8DEFCB;
+    &::after
+      background: linear-gradient(to right, #8DEFCB 0%, #D8D8D8 33.33%);
 .main-container
   max-width: 628px;
   margin: 0 auto;
@@ -90,6 +114,23 @@ export default {
   left: -234px;
   text-align: left;
   line-height: 24px;
+  &__title
+    font-size: 18px;
+    color: #000000;
+    letter-spacing: 1.88px;
+  &__button
+    background: #4B4B4B;
+    border-radius: 5px;
+    color: #fff;
+    display: inline-block;
+    padding: 10px 14px;
+    margin-top: 24px;
+    cursor: pointer;
+    font-size: 14px;
+    line-height: 19px;
+    letter-spacing: 1.46px;
+    min-width: 90px;
+    text-align: center;
 .order-container
   background: #F3F3F3;
   border-radius: 10px 0 0 10px;
@@ -100,11 +141,28 @@ export default {
   border-radius: 0 10px 0 0;
   padding: 38px 58px;
   height: 100%
+  text-align: right;
   &__title
     font-size: 20px;
-    color: #000000;
+    color: #000;
     letter-spacing: 2.09px;
     margin-bottom: 90px;
+    position: relative;
+    z-index: 2;
+    display: block;
+    padding: 0 10px;
+    width: 240px;
+    margin: 0 auto 90px;
+    &::after
+      content: '';
+      position: absolute;
+      width: 100%;
+      display: block;
+      bottom: -5px;
+      left: 0;
+      z-index: -1;
+      height: 17px;
+      background: #B5FFE4;
 .radio-container
   display: flex;
   flex-wrap: wrap;
@@ -121,10 +179,31 @@ export default {
     flex-direction: column;
     align-items: center;
     justify-content: flex-end;
+    cursor: pointer;
+    transition: background .35s ease;
   &__title
     margin-top: 10px;
   &__control
     display: none;
-    &:checked + .radio__item
-      background: #36B996;
+    &:checked
+      & + ^[0]
+        &__item
+          background: #36B996;
+          & ^[-4]__icon
+            filter: brightness(100);
+          & ^[-4]__title
+            color: #fff;
+.shop-button
+  background: #36B996;
+  border-radius: 5px;
+  color: #fff;
+  display: inline-block;
+  padding: 10px 14px;
+  margin-top: 24px;
+  cursor: pointer;
+  font-size: 14px;
+  line-height: 19px;
+  letter-spacing: 1.46px;
+  min-width: 90px;
+  text-align: center;
 </style>
